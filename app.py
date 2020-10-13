a152fa5e8b0c637b517e9ff423b68d33e729b340
Colcothar <33038729+Colcothar@users.noreply.github.com>
2020-10-13 19:33:23 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,41 @@
+import discord 
+from discord.ext import commands
+from magichome import MagicHomeApi
+import datetime
+
+
+
+client = commands.Bot(command_prefix=">")
+
+def check(num):
+    if num<256 and num>-1:
+        return True
+    else:
+        return False
+
+
+@client.event
+async def on_ready():
+    print("Bot ready")
+    
+@client.command()
+async def status(ctx):
+    await ctx.send("@everyone If you hate the bot owner, change the colour of the lights in their room? \nCall it with >LED and provide the RGB colours. Simple eh? ie *>LED 255 255 255* ")
+    
+@client.command()
+async def LED(ctx, arg, arg1, arg2):
+    controller = MagicHomeApi('192.168.1.112', 0)
+    print(round(int(arg)), round(int(arg1)), round(int(arg2)))
+    if(check(round(int(arg))) and check(round(int(arg1))) and check(round(int(arg2)))):
+        controller.update_device(round(int(arg)), round(int(arg1)), round(int(arg2)))
+        await ctx.send('Lights set!')
+    else:
+         await ctx.send('Errrr they aint rgb values? ')
+  
+
+
+
+
+
+    
+client.run(token)
